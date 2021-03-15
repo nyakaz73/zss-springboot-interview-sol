39eabdfb18f0b7babaac179dbf8e6edeb056c011
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-03-15 16:57:06 +0200

    Created enums for Types and ExtendedTypes

---
+++ src/main/java/com/zss/interview/springbootrestapi/enums/ExtendedTypes.java
@@ -0,0 +1,10 @@
+package com.zss.interview.springbootrestapi.enums;
+
+public enum ExtendedTypes {
+    EXTENDED_TYPE("NONE");
+    private String name;
+
+    ExtendedTypes(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/zss/interview/springbootrestapi/enums/Types.java
@@ -0,0 +1,12 @@
+package com.zss.interview.springbootrestapi.enums;
+
+public enum Types {
+    TYPE("PURCHASE");
+
+    private final String name;
+    Types(String name) {
+        this.name = name;
+    }
+
+
+}
